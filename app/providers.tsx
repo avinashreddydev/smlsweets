ff1fff9ce9c1fc2bf17157ee6fa1a8716f60e681
Avinash Reddy <avinashreddy.idio@gmail.com>
2025-12-20 02:25:22 -0500

    previous phone auth error corrected

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
-import { PhoneAuthDrawer } from "@/components/auth/PhoneLoginDrawer";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     const [queryClient] = useState(() => new QueryClient());
